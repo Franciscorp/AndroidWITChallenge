4c4d7baf01af002416762a5175ea177f8428c58c
Franciscorp <franciscorp1994@hotmail.com>
2021-07-19 00:57:14 +0100

    After experimentation. UI is now updated when response is received

--- AndroidWitChallenge/app/src/main/java/pt/franciscorp/wit_challenge/MainActivity.java
+++ AndroidWitChallenge/app/src/main/java/pt/franciscorp/wit_challenge/MainActivity.java
@@ -3,11 +3,14 @@ package pt.franciscorp.wit_challenge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textViewWeatherInfo;
+    String weatherInfo = "";
+
 
     OpenWeatherMapCommunication weatherCommunication;
 
@@ -17,14 +20,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //15m planning on paper. 1 class. 2 basic drawing of UI
+        //35 of setting up the first request. Not tested
+        //47 minutes, UI is updated
         //UI START UP
-        String weatherInfo = "";
         textViewWeatherInfo = findViewById(R.id.TextViewWeatherInfo);
 
         //VARS START UP
-        weatherCommunication.getWeatherData("Coimbra", null, Constants.UnitsOfMeasure.METRIC);
+        weatherCommunication = new OpenWeatherMapCommunication("Coimbra", null, Constants.UnitsOfMeasure.METRIC);
 
-        textViewWeatherInfo.setText(weatherInfo);
+
+//        Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            public void run() {
+//                weatherInfo = weatherCommunication.getWeatherData("Coimbra", null, Constants.UnitsOfMeasure.METRIC);
+////                weatherInfo = weatherCommunication.jsonObtained;
+//                textViewWeatherInfo.setText(weatherInfo);
+//            }
+//        }, 5000);   //5 seconds
+//
+//
+//        new Thread(new Runnable() {
+//            public void run() {
+//                weatherInfo = weatherCommunication.getWeatherData("Coimbra", null, Constants.UnitsOfMeasure.METRIC);
+//                try{
+//                    textViewWeatherInfo.setText(weatherInfo);
+//                }catch(Exception e){
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
+
+        callWeatherApi();
 
     }
+
+    public void callWeatherApi() {
+        new Thread(new Runnable() {
+            public void run() {
+                weatherInfo = weatherCommunication.getWeatherData("Coimbra", null, Constants.UnitsOfMeasure.METRIC);
+
+                textViewWeatherInfo.post(new Runnable() {
+                    public void run() {
+                        textViewWeatherInfo.setText(weatherInfo);
+                    }
+                });
+            }
+        }).start();
+    }
+
+
 }
--- AndroidWitChallenge/app/src/main/java/pt/franciscorp/wit_challenge/OpenWeatherMapCommunication.java
+++ AndroidWitChallenge/app/src/main/java/pt/franciscorp/wit_challenge/OpenWeatherMapCommunication.java
@@ -9,10 +9,18 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class OpenWeatherMapCommunication {
+import kotlinx.coroutines.Dispatchers;
+
+public class OpenWeatherMapCommunication{
 //    public String openWeatherApiLink = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=";
-    public String openWeatherApiLink = "http://api.openweathermap.org/data/2.5/weather?q=";
+    public String openWeatherApiLink = "https://api.openweathermap.org/data/2.5/weather?q=";
     public String apiKey = "a6f443d25b2fc78fd00b664f9174bcb5";
+    public String jsonObtained;
+
+    public String cityName;
+    public String language;
+    public Constants.UnitsOfMeasure units;
+
     //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     //city iso names
     //https://www.iso.org/obp/ui/#search
@@ -20,7 +28,14 @@ public class OpenWeatherMapCommunication {
     //	Units of measurement. standard, metric and imperial units are available. If you do not use the units parameter, standard units will be applied by default. Learn more
 
 
+    public OpenWeatherMapCommunication(String cityName, String language, Constants.UnitsOfMeasure units) {
+        this.cityName = cityName;
+        this.language = language;
+        this.units = units;
+    }
+
     //language and units will be const, but left here for future adaption to other countries
+//    public String getWeatherData(String cityName, String language, Constants.UnitsOfMeasure units) {
     public String getWeatherData(String cityName, String language, Constants.UnitsOfMeasure units) {
         HttpURLConnection con = null ;
         InputStream is = null;
@@ -33,6 +48,9 @@ public class OpenWeatherMapCommunication {
                 units = Constants.UnitsOfMeasure.METRIC;
             if (language != null)
                 url = url + "&lang=" + language+"&units" + units + "&APPID=" + apiKey;
+            else
+                url = url + "&units=" + units + "&APPID=" + apiKey;
+
             Log.d("Tag","URL " + url);
 
             con = (HttpURLConnection) ( new URL(url)).openConnection();
@@ -56,6 +74,7 @@ public class OpenWeatherMapCommunication {
             is.close();
             con.disconnect();
 
+            jsonObtained = buffer.toString();
             return buffer.toString();
         }
         catch(Throwable t) {
@@ -68,6 +87,4 @@ public class OpenWeatherMapCommunication {
         return null;
 
     }
-
-
 }
