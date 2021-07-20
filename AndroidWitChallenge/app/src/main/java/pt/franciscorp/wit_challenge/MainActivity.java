50e35892f534c53a5e4c265b8b706519c9c2ed9c
Franciscorp <franciscorp1994@hotmail.com>
2021-07-20 02:49:10 +0100

    Change of themes, dark theme and Title

--- AndroidWitChallenge/app/src/main/java/pt/franciscorp/wit_challenge/MainActivity.java
+++ AndroidWitChallenge/app/src/main/java/pt/franciscorp/wit_challenge/MainActivity.java
@@ -1,13 +1,18 @@
 package pt.franciscorp.wit_challenge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -26,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        this.setTitle("");
+        setLayoutForApp();
 
         //15m planning on paper. 1 class. 2 basic drawing of UI
         //35 of setting up the first request. Not tested
@@ -34,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         //22 m to Get All json data from api
         //47 minutes for json parser
         //15m for icon
+        //60m for current location
+        //10m
         //UI START UP
         textViewWeatherInfo = findViewById(R.id.TextViewWeatherInfo);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
@@ -44,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
             return;
         }
+
         fusedLocationClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
@@ -68,6 +79,20 @@ public class MainActivity extends AppCompatActivity {
 
 //        callWeatherApi(weatherData.cityWeatherArrayList.get(0));
         System.out.println();
+    }
+
+    private void setLayoutForApp() {
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//        View decorView = getWindow().getDecorView();
+//        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+//        decorView.setSystemUiVisibility(uiOptions);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
     }
 
     public void getAllCitiesWeatherFromApi(){
